ac86fc042537bc8b0dd04fe3d39ac957a6569ac6
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2020-11-09 13:33:38 -0300

    adicionando funcionalidade de Salvar evento no BD

--- src/main/java/com/appeventos/appeventos/controllers/EventoController.java
+++ src/main/java/com/appeventos/appeventos/controllers/EventoController.java
@@ -1,14 +1,28 @@
 package com.appeventos.appeventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.appeventos.appeventos.models.Evento;
+import com.appeventos.appeventos.repository.IEventoRepository;
 
 @Controller
 public class EventoController {
 	
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private IEventoRepository er;
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET) //get, vai retornar o formulario
 	public String form() {
 		return "eventos/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST) //post pra quando for salvar no BD
+	public String form(Evento evento) {
+		er.save(evento);
+		return "redirect:/cadastrarEvento"; //apos salvar, vai redirecionar para o formulario
 	}
 
 }
---
+++ src/main/java/com/appeventos/appeventos/repository/IEventoRepository.java
@@ -0,0 +1,10 @@
+package com.appeventos.appeventos.repository;
+
+import com.appeventos.appeventos.models.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IEventoRepository extends CrudRepository<Evento, String> {
+	
+	
+
+}
