21d357248b6d157bf677722c8277173113238b23
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2020-12-28 14:29:31 -0300

    adicionando pagina de login customizada

--- src/main/java/com/appeventos/appeventos/security/WebSecurityConfig.java
+++ src/main/java/com/appeventos/appeventos/security/WebSecurityConfig.java
@@ -21,12 +21,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/").permitAll()
-		.antMatchers(HttpMethod.GET, "/cadastrar-novo-usuario").permitAll()
-		.antMatchers(HttpMethod.POST, "/cadastrar-novo-usuario").permitAll()
-		.anyRequest().authenticated()
-		.and().formLogin().permitAll()
-		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		.antMatchers(HttpMethod.GET, "/")
+			.permitAll()
+		.antMatchers(HttpMethod.GET, "/cadastrar-novo-usuario")
+			.permitAll()
+		.antMatchers(HttpMethod.POST, "/cadastrar-novo-usuario")
+			.permitAll()
+		.anyRequest()
+			.authenticated()
+		.and()
+			.formLogin()
+			.loginPage("/login")
+			.permitAll()
+		.and()
+			.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 
 	@Override
